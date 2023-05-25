f68a591aafb817796bb7d92bae983980debb9a95
Kyle <kyleslaughter8@gmail.com>
2023-05-25 17:58:27 -0600

    finished* project. Some things are still slightly modified for testing

--- script.js
+++ script.js
@@ -1,23 +1,79 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  var dateEl = $('#currentDay');
+  var buttons = $('.saveBtn');
+  var hours = $('.time-block');
+  var clearBtn = $('#clearBtn');
+
+  time();
+  var dateUpdater = setInterval(time, 1000);
+
+  populateText();
+
+  function time (){
+    var currentDate = new dayjs();
+    dateEl.text(currentDate.format('ddd, MMM D - h:mm:ss A'));
+    setState(currentDate.format('H') - 4);
+  }
+  
+  function saveData (data){
+    localStorage.setItem("data", JSON.stringify(data));
+  }
+  
+  function getData (){
+    var data = JSON.parse(localStorage.getItem("data"));
+    if(data !== null){
+      return data;
+    }else{
+      return [];
+    }
+  }
+
+  function setState(hour){
+    for(var i = 9; i < 18; i++){
+      if(i < hour){
+        hours.eq(i - 9).addClass('past');
+      }else if(i > hour){
+        hours.eq(i - 9).addClass('future');
+      }else{
+        hours.eq(i - 9).addClass('present');
+      }
+    }
+  }
+
+  buttons.on('click', function (event) {
+    var clicked = $(event.target);
+    if(clicked.is('i')){
+      clicked = clicked.parent();
+    }
+    var textField = clicked.siblings(".description");
+    var text = textField.val();
+
+    var hourClicked = clicked.parent().attr('id');
+    hourClicked = hourClicked.substring(hourClicked.length - 2, hourClicked.length);
+    console.log(hourClicked);
+    
+    var data = getData();
+    data.push({hour: hourClicked, plan: text});
+    saveData(data);
+  });
+
+  clearBtn.on('click', function () {
+    var reset = [];
+    saveData(reset);
+    var editEl = $('#hour-09');
+    editEl.children('.description').eq(0).text("");
+    for(var i = 10; i < 18; i++){
+      editEl = $('#hour-' + i);
+      editEl.children('.description').eq(0).text("");
+    }
+  });
+
+  function populateText() {
+    var data = getData();
+    for(var i = 0; i < data.length; i++){
+      var editEl = $('#hour-' + data[i].hour);
+      editEl.children('.description').eq(0).text(data[i].plan);
+    }
+  }
 });
+
